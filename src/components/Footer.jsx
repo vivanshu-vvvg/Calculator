4f9d87aabf9ae117c41f32081633a11f52d86e91
vivanshu-vvvg <vivanshugupta2003.m@gmail.com>
2023-02-09 20:27:45 +0530

    Adding Footer, Navbar logo and some CSS

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+
+import './Footer.css';
+import React from 'react'
+// className='bg-dark text-light py-3 text-center'
+const Footer = () => {
+
+  return (
+    <div>
+    <nav className='nav_container'>
+      <div className='nav_left'>
+          <a href='#home'>VG</a>
+    </div>
+    </nav>
+    <footer className='footer'>
+      <p>
+        Vivanshu &copy; MyCalculator
+      </p>
+      </footer>
+      </div>
+  )
+}
+
+export default Footer ;
